f4914c095ae5604368b26ff19a48b95f17b83985
mars <even_zhang@edge-core.com>
2020-12-17 14:08:09 +0800

    Add mininet topology customize file

--- custom/topo-2sw-6host.py
+++ custom/topo-2sw-6host.py
@@ -20,15 +20,26 @@ class MyTopo( Topo ):
         Topo.__init__( self )
 
         # Add hosts and switches
-        leftHost = self.addHost( 'h1' )
-        rightHost = self.addHost( 'h2' )
-        leftSwitch = self.addSwitch( 's3' )
-        rightSwitch = self.addSwitch( 's4' )
+        h1 = self.addHost( 'h1' )
+        h2 = self.addHost( 'h2' )
+        h3 = self.addHost( 'h3' )
+        s1 = self.addSwitch( 's1' )
+
+        h4 = self.addHost( 'h4' )
+        h5 = self.addHost( 'h5' )
+        h6 = self.addHost( 'h6' )
+        s2 = self.addSwitch( 's2' )
 
         # Add links
-        self.addLink( leftHost, leftSwitch )
-        self.addLink( leftSwitch, rightSwitch )
-        self.addLink( rightSwitch, rightHost )
+        self.addLink( h1, s1 )
+        self.addLink( h2, s1 )
+        self.addLink( h3, s1 )
+
+        self.addLink( s1, s2 )
+
+        self.addLink( h4, s2 )
+        self.addLink( h5, s2 )
+        self.addLink( h6, s2 )
 
 
 topos = { 'mytopo': ( lambda: MyTopo() ) }
